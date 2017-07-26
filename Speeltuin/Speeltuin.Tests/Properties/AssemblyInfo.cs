076c01337b8b3addd94c9f6ca64bf114fba20f9b
Gino Bos <bogi@pggm.nl>
2017-07-26 17:46:43 +0200

    Added some tests for encryption and hashing

---
+++ Speeltuin/Speeltuin.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Speeltuin.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Speeltuin.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("042b0970-b520-46d5-952d-1bb2e5fbc8b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Speeltuin/Speeltuin.Tests/SecurityTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Speeltuin.Tests
+{
+    [TestClass]
+    public class SecurityTests
+    {
+        [TestMethod]
+        public void Sha256HashTest()
+        {
+            const string password = "supers3retPass!@";
+            var bytes = Encoding.Unicode.GetBytes(password);
+            var hash = SHA256.Create().ComputeHash(bytes);
+
+            Assert.IsTrue(hash.Length > 20);
+        }
+
+        [TestMethod]
+        public void AesEncryptionAndDecryptionTest()
+        {
+            //Encrypt data
+            using (var aes = new AesManaged())
+            {
+                aes.IV = new byte[aes.BlockSize / 8];
+
+                const string secret = "secret!!";
+                var orignalBytes = Encoding.Unicode.GetBytes(secret);
+                byte[] encryptdBytes;
+
+                using (var ms = new MemoryStream())
+                using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(orignalBytes, 0, orignalBytes.Length); //Writes the bytes to the cryptostream
+                    cs.FlushFinalBlock();
+                    encryptdBytes = ms.ToArray();
+                }
+
+                //Decrypt data
+                string resultAfterEncryption;
+                using (var ms = new MemoryStream())
+                using (var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(encryptdBytes, 0, encryptdBytes.Length);
+                    resultAfterEncryption = Encoding.Unicode.GetString(ms.ToArray());
+                }
+
+                Assert.AreEqual(secret, resultAfterEncryption);
+            }
+        }
+    }
+}
